d37df96319001db0086460afd7f55f086bf38abd
Nataliya Statova <natasha_pupkina@mail.ru>
2023-09-29 15:08:22 +0300

    Задача исправлена

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,12 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.security.Provider;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
